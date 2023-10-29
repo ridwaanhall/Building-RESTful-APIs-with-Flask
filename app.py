91dfa21d5c0a007339c10c92147c2a92fbf4c0f7
ridwaanhall <erbyl14@gmail.com>
2023-10-29 10:00:10 +0700

    Adds remove_planet and requirements.txt

--- app.py
+++ app.py
@@ -217,6 +217,18 @@ def update_planet():
         return jsonify(message="That planet does not exist"), 404
 
 
+@app.route('/remove_planet/<int:planet_id>', methods=['DELETE'])
+@jwt_required()
+def remove_planet(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(message="You deleted a planet"), 202
+    else:
+        return jsonify(message="That planet does not exist"), 404
+
+
 # database models
 class User(db.Model):
     __tablename__ = 'users'
