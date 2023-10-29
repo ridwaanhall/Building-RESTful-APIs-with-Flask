3ff932a1f537377690aed8a7d8bfd2fb576fade3
ridwaanhall <erbyl14@gmail.com>
2023-10-29 09:09:18 +0700

    Adds planet detail

--- app.py
+++ app.py
@@ -164,6 +164,16 @@ def retrieve_password(email: str):
         return jsonify(message="That email doesn't exist"), 401
 
 
+@app.route('/planet_details/<int:planet_id>', methods=["GET"])
+def planet_details(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        result = planet_schema.dump(planet)
+        return jsonify(result)
+    else:
+        return jsonify(message="That planet does not exist"), 404
+
+
 # database models
 class User(db.Model):
     __tablename__ = 'users'
